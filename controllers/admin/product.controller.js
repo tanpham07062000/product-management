c2d582b6806df13307074663d3c7ae1dd4c6e3a4
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-01 22:16:52 +0700

    LLấy ra sản phẩm bên admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,12 @@
+const Product = require("../../models/product.model");
 // [GET] /admin/products
-module.exports.index=(req, res) => {
-  res.render("admin/pages/products/index",{
-    pageTitle:"Products List"
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
   });
-}
+  // console.log(products);
+  res.render("admin/pages/products/index", {
+    pageTitle: "Products List",
+    products: products,
+  });
+};
