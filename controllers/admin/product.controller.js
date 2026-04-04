dda510055f52a750a35291d49ac6e47bbcf85da5
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-04 11:14:23 +0700

    Admin tối ưu phần chức năng  search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,22 +1,22 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  
-  const filterStatus=filterStatusHelper(req.query);
-
   let find = {
     deleted: false,
   };
+
+  //tinh nang filterStatus
+  const filterStatus=filterStatusHelper(req.query);
   if (req.query.status) {
     find.status = req.query.status;
   }
 
-  let keyword = "";
-  if (req.query.keyword) {
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword,"i");
-    find.title = regex;
+// Tinh nang search
+  const objectSearch = searchHelper(req.query); 
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
   }
 
   const products = await Product.find(find);
@@ -25,6 +25,6 @@ module.exports.index = async (req, res) => {
     pageTitle: "Products List",
     products: products,
     filterStatus: filterStatus,
-    keyword:keyword,
+    keyword:objectSearch.keyword,
   });
 };
