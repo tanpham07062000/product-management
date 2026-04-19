40123d290994afd89600844d8aa500dc9edd3ce5
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-19 08:19:16 +0700

    Tạo mới sản phẩm-chọn ảnh

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 var methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-require('dotenv').config();
-// tao thong bao
 var flash = require('express-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-//kết nối MongoDB
 const database = require("./config/database");
-database.connect();
-
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
+const systemConfig = require("./config/system");
+
+
+
+require('dotenv').config();
+
+// tao thong bao
+
+
+//kết nối MongoDB
+
+database.connect();
+
+
 const app = express();
 const port = process.env.PORT;
 
@@ -28,7 +37,7 @@ app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 
 // App Locals Variables
-const systemConfig = require("./config/system")
+
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 //Nhúng file tĩnh
