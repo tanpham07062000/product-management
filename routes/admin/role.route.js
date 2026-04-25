8532a686caa9eed959a72ce583c74d67e25c2cf0
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-25 15:47:03 +0700

    Tính năng chỉnh sửa nhóm quyền

--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -6,4 +6,7 @@ router.get("/", controller.index);
 router.get("/create", controller.create);
 router.post("/create", controller.createPost);
 
+router.get("/edit/:id", controller.edit);
+router.patch("/edit/:id", controller.editPatch);
+
 module.exports = router;
