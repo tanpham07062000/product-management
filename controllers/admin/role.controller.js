5a8b00cde0337d7259cd1638678a71f7fdd75df8
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-26 15:18:38 +0700

    Xây dựng phần Phân quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -55,3 +55,30 @@ module.exports.editPatch = async (req, res) => {
     req.flash("error", " Cập nhật nhóm quyền thất bại!");
   }
 };
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+  const records = await Role.find(find);
+  
+  res.render("admin/pages/roles/permissions", {
+    pageTitle: "Phân quyền",
+    records: records,
+  });
+};
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+  try{
+  const permissions = JSON.parse(req.body.permissions);
+  for (const item of permissions) {
+    await Role.updateOne({ _id: item.id }, { permissions: item.permissions });
+  }
+req.flash("success", "Cập nhật phân quyền thành công!");
+  res.redirect(req.get("Referrer") || "/");
+  }
+  catch (error){
+    req.flash("error", "Cập nhật phân quyền thất bại!");
+  }
+
+};
