06b2fc97da6c4c09b25e22fdcc6d90d77396f884
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-28 21:17:23 +0700

    Thêm middleware cho route private

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,19 +1,21 @@
 const { ReturnDocument } = require("mongodb");
 
-
 const Account = require("../../models/account.model");
 const md5 = require("md5");
 
-
 const systemConfig = require("../../config/system");
 // [GET] /admin/auth/login
-module.exports.login=(req, res) => {
-  res.render("admin/pages/auth/login",{
-    pageTitle:"Đăng nhập",
-  });
-}
+module.exports.login = async (req, res) => {
+  if (req.cookies.token) {
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+  } else {
+    res.render("admin/pages/auth/login", {
+      pageTitle: "Đăng nhập",
+    });
+  }
+};
 // [POST] /admin/auth/login
-module.exports.loginPost= async (req, res) => {
+module.exports.loginPost = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const user = await Account.findOne({
@@ -21,29 +23,29 @@ module.exports.loginPost= async (req, res) => {
     deleted: false,
   });
 
-  if(!user){
-    req.flash("error","Email không tồn tại!");
+  if (!user) {
+    req.flash("error", "Email không tồn tại!");
     res.redirect(req.get("Referrer") || "/");
     return;
   }
 
-  if(md5(password) !== user.password){
-    req.flash("error","Sai mật khẩu");
+  if (md5(password) !== user.password) {
+    req.flash("error", "Sai mật khẩu");
     res.redirect(req.get("Referrer") || "/");
     return;
   }
-  if(user.status == "inactive"){
-    req.flash("error","Tài khoản đã bị khóa!");
+  if (user.status == "inactive") {
+    req.flash("error", "Tài khoản đã bị khóa!");
     res.redirect(req.get("Referrer") || "/");
     return;
   }
   res.cookie("token", user.token);
   res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
-}
+};
 
 // [GET] /admin/auth/logout
-module.exports.logout=(req, res) => {
+module.exports.logout = (req, res) => {
   // xóa token trong cookie
   res.clearCookie("token");
   res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
-}
+};
