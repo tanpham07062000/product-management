c6efa733c822b874b77d534c605742e79b29a844
tanpham07062000 <tanpham07062000@gmail.com>
2026-04-28 19:51:37 +0700

    Tính năng đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 const systemConfig = require("../../config/system");
 module.exports = (app) => {
   const PATH_ADMIN= systemConfig.prefixAdmin;
@@ -11,4 +12,5 @@ module.exports = (app) => {
   app.use(PATH_ADMIN +"/products-category",productCategoryRoutes);
   app.use(PATH_ADMIN +"/roles",roleRoutes);
   app.use(PATH_ADMIN +"/accounts",accountRoutes);
+  app.use(PATH_ADMIN +"/auth",authRoutes);
 };
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,14 @@
+module.exports.loginPost = (req,res,next)=>{
+  if(!req.body.email){
+    req.flash("error", `Vui lòng nhập email!`);
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  if(!req.body.password){
+    req.flash("error", `Vui lòng nhập mật khẩu!`);
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  
+  next();
+}
