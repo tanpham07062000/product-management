3d607e6b36f022461f958eb5f8481a3cef0f4ef7
tanpham07062000 <tanpham07062000@gmail.com>
2026-05-31 23:12:28 +0700

    Hiển thị sản phẩm mới bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,20 +1,29 @@
-const Product=require("../../models/product.model");
+const Product = require("../../models/product.model");
 
-const productsHelper=require("../../helpers/products");
+const productsHelper = require("../../helpers/products");
 // [GET] /
 
 module.exports.index = async (req, res) => {
   //Lấy ra sản phẩm nổi bật
-  const productsFeatured=await Product.find({
-    featured:"1",
+  const productsFeatured = await Product.find({
+    featured: "1",
     deleted: false,
-    status:"active",
+    status: "active",
   }).limit(6);
-  const newProducts = productsHelper.newPriceProducts(productsFeatured);
+  const newProductsFeatured = productsHelper.newPriceProducts(productsFeatured);
   // Hết lấy ra sản phẩm nổi bật
-
+  // Hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductsNew=productsHelper.newPriceProducts(productsNew);
+  // Hết Hiển thị danh sách sản phẩm mới nhất
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured:newProducts,
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
