f08eb3907d27802b72c7da76c522f0f7d8eb1f6b
tanpham07062000 <tanpham07062000@gmail.com>
2026-05-31 20:52:57 +0700

    Hiển thị danh mục sản phẩm client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,11 @@
+
 // [GET] /
 
-module.exports.index=(req, res) => {
-  res.render("client/pages/home/index",{
-    pageTitle:"Home"
+module.exports.index = async (req, res) => {
+
+
+  res.render("client/pages/home/index", {
+    pageTitle: "Trang chủ",
+    
   });
-}
+};
