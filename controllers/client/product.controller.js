99231a7e9a53dcfadbc7a5a62f91a394427b3a32
tanpham07062000 <tanpham07062000@gmail.com>
2026-06-01 00:34:59 +0700

    Lấy ra các sản phẩm theo danh mục trong client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,8 @@
 const Product = require("../../models/product.model");
-const productsHelper=require("../../helpers/products");
+const ProductCategory = require("../../models/product-category.model");
+
+const productsHelper = require("../../helpers/products");
+const productsCategoryHelper = require("../../helpers/products-category");
 // [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
@@ -23,13 +26,43 @@ module.exports.detail = async (req, res) => {
       status: "active",
     };
     const product = await Product.findOne(find);
-    
+
     res.render("client/pages/products/detail", {
       pageTitle: product.title,
       product: product,
     });
   } catch (error) {
     req.flash("error", `Không tồn tại sản phẩm này!`);
-    res.redirect(`/products`);
+    res.redirect(req.get("Referrer") || "/");
+  }
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  try {
+    const category = await ProductCategory.findOne({
+      slug: req.params.slugCategory,
+      status: "active",
+      deleted: false,
+    });
+    // Lấy tất cả các id con vào 1 mảng
+    const listSubCategory = await productsCategoryHelper.getSubCategory(
+      category.id,
+    );
+    const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+    const products = await Product.find({
+      product_category_id: { $in: [category.id, ...listSubCategoryId] },
+      deleted: false,
+    }).sort({ position: "desc" });
+
+    const newProducts = productsHelper.newPriceProducts(products);
+    res.render("client/pages/products/index", {
+      pageTitle: category.title,
+      products: newProducts,
+    });
+  } catch (error) {
+    req.flash("error", `Không tồn tại danh mục sản phẩm này!`);
+    res.redirect(req.get("Referrer") || "/");
   }
 };
--- middlewares/client/category.middleware.js
+++ middlewares/client/category.middleware.js
@@ -2,6 +2,7 @@ const ProductCategory = require("../../models/product-category.model");
 const createTreeHelper = require("../../helpers/createTree");
 module.exports.category = async (req, res, next) => {
   const productsCategory = await ProductCategory.find({
+    status: "active",
     deleted: false,
   });
   const newProductsCategory = createTreeHelper.tree(productsCategory);
