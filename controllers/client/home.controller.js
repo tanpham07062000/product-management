09d0b95bfbdbe654acb6dec47a351dbc7e865b4b
tanpham07062000 <tanpham07062000@gmail.com>
2026-05-31 22:59:40 +0700

    Thêm vào trang chủ các sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,11 +1,20 @@
+const Product=require("../../models/product.model");
 
+const productsHelper=require("../../helpers/products");
 // [GET] /
 
 module.exports.index = async (req, res) => {
-
+  //Lấy ra sản phẩm nổi bật
+  const productsFeatured=await Product.find({
+    featured:"1",
+    deleted: false,
+    status:"active",
+  }).limit(6);
+  const newProducts = productsHelper.newPriceProducts(productsFeatured);
+  // Hết lấy ra sản phẩm nổi bật
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    
+    productsFeatured:newProducts,
   });
 };
