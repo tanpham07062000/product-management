8db0e9e22a8d11898abb653290072473d0c2b34b
tanpham07062000 <tanpham07062000@gmail.com>
2026-05-31 22:01:40 +0700

    Bảo mật để cho không dùng postman gửi linh tinh lên được

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -152,17 +152,23 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-  if (req.body.position == "") {
-    const count = await ProductCategory.countDocuments();
-    req.body.position = count + 1;
+  const permissions = res.locals.role.permissions;
+  if (permissions.includes("products-category_create")) {
+    if (req.body.position == "") {
+      const count = await ProductCategory.countDocuments();
+      req.body.position = count + 1;
+    } else {
+      req.body.position = parseInt(req.body.position);
+    }
+
+    const record = new ProductCategory(req.body);
+    await record.save();
+
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
   } else {
-    req.body.position = parseInt(req.body.position);
+    res.send("403");
+    return;
   }
-
-  const record = new ProductCategory(req.body);
-  await record.save();
-
-  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
 
 // [GET] /admin/products-category/edit/:id
